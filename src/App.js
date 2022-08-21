9cde203e5b57d7ae0c9ea004b1e7403301a12438
Crystal Knapp <rc_knapp@msn.com>
2022-08-20 21:15:47 -0400

    challenge 1 - Crystal's solution

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
+import Challenge1 from './components/Challenge1'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
 
+        <Route path='/challenge1' element={<Challenge1 />} />
 
       </Routes>
 
--- src/components/Challenge1.js
+++ src/components/Challenge1.js
@@ -5,3 +5,70 @@
 //add a CSS rule to the App.css file so the component's body is indented.
 //Add a route/link for this component to the Header component.
 
+import { useEffect, useState } from "react";
+
+import players from '../data/players'
+
+const Challenge1 = () => {
+    const [playersData, setPlayersData] =  useState(players)
+    const [playerList, setPlayerList] = useState('') 
+    const namesToString = () => {
+        setPlayerList(
+            playersData.reduce((acc, cur)=>{
+                if(acc){
+                    acc+= ', ';
+                }
+                acc+=cur.name ;
+                return acc;
+                },'')
+        )
+    }   
+    useEffect(()=>{
+        namesToString();
+    })
+
+    const sortAscending = () => {
+        setPlayersData(playersData.sort((a,b)=> {
+            if(a.name.toUpperCase() < b.name.toUpperCase()){
+                return -1;
+            }
+            else {
+                return 1;
+            }
+        }))
+        namesToString();
+    }
+
+
+    const sortDescending = () => {
+        setPlayersData(playersData.sort((a,b)=> {
+            if(a.name.toUpperCase() > b.name.toUpperCase()){
+                return -1;
+            }
+            else {
+                return 1;
+            }
+        }))
+        
+        namesToString();
+    }
+  return (
+    <div className='challenge1'>
+        <div>
+            <h2>Challenge 1</h2>
+            <br />
+            <h5>Names of Players:</h5>
+            <p>{playerList}</p>
+        </div>
+        <br />
+        <div>
+            <button onClick={sortAscending}>Sort Ascending</button>    
+            {'  '}
+            <button onClick={sortDescending}>Sort Descending</button>    
+        </div>
+        
+    </div>
+  )
+}
+
+export default Challenge1
