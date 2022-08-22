de7e8cb1e178550eff454e2fe311f647457050af
mrdeangray <mrdeangray@gmail.com>
2022-08-21 20:27:16 -0400

    created Challenge3

---
+++ src/components/Challenge3.js
@@ -0,0 +1,9 @@
+
+//Challenge3:
+//Create a Challenge3 component in the components directory.
+//the component should retrieve and display the names from the players.js file in the data directory.
+//the names should be grouped by the team, both by rows and columns.
+//Add CSS rules to change the text color and background colors to match the team's color.
+//add a CSS rule to the App.css file so the component's body is indented.
+//Add a route/link for this component to the Header component.
+
