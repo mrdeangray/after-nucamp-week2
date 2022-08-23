51359f5c8e146d547c80fb984b3bbc7866a44dce
Crystal Knapp <rc_knapp@msn.com>
2022-08-23 09:32:44 -0400

    Challenge 4 with crystal's answers

---
+++ src/components/Archive.js
@@ -0,0 +1,10 @@
+// use data from archives.js in the data folder
+// -create an archive page for storing the previous challenges
+// -the archive should have a link to the pages the challnges
+// (the data in the archives.js file and currently have to 
+// be manually added)/ ex: /challenge1
+// -the archive pages should show the title of the challenge 
+// (ex: Challenge 1), with a description (again, manually added to 
+// the archives.js)
+// -remember to add the make sure the links are all active and working
+// -add a link to the archive page to the header
