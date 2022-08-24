480afb912545e4d16d22b4c21b819896f53e35fa
Crystal Knapp <rc_knapp@msn.com>
2022-08-23 22:56:31 -0400

    Challenge 5 instructions

---
+++ src/components/Challenge5.js
@@ -0,0 +1,20 @@
+/*
+    *** Challenge 5 ***
+    For this challenge, you will create a search feature
+    for the archives
+    
+    When the user clicks on a search-by-keyword button, it 
+    should return a list of all the keywords in all the 
+    challenges. The list should only one of each keyword (no
+    duplicates).  *** note that there is now a keyword array
+    in the ./data/archives.js file for this purpose. there is 
+    no need to search the actual code ***
+
+    The keywords (or phrases) displayed on the screen 
+    should be clickable. Then, when the user clicks on 
+    the keyword, it should then compile a list of every 
+    challenge that has that keyword.
+
+    Update App.css as needed. 
+
+*/
--- src/data/archives.js
+++ src/data/archives.js
@@ -2,7 +2,8 @@ const previousChallengeList = [
     {
         title: "Challenge 1",
         location: '/challenge1',
-        description: 'Players, sort alphabetically'
+        description: 'Players, sort alphabetically',
+        keywords: ['sort', 'reduce', 'useState', 'useEffect']
     }, {
         title: "Challenge 2",
         location: '/challenge2',
@@ -12,13 +13,21 @@ const previousChallengeList = [
     }, {
         title: "Challenge 3",
         location: '/challenge3',
-        description: 'Players, sort by team (color-coded)'
+        description: 'Players, sort by team (color-coded)',
+        keywords: ['sort', 'reduce', 'useState', 'useEffect']
     }, {
         title: "Challenge 4",
         location: '/challenge4',
         description: 'Create an archive containing previous challenges',
         keywords: ['map']
-    }
+    },{
+        title: "Challenge 5",
+        location: '/challenge5',
+        description: 'Create a linked list of all the keywords in the challenge archives',
+        keywords: ['map']
+    },
+    
+    
 ]
 
 export default previousChallengeList;
