98418961f44c01ead39c13e45d761e1b734d94cc
Crystal Knapp <rc_knapp@msn.com>
2022-08-23 22:22:58 -0400

    challenge 4

--- src/data/archives.js
+++ src/data/archives.js
@@ -2,20 +2,24 @@ const previousChallengeList = [
     {
         title: "Challenge 1",
         location: '/challenge1',
-        description: 'Players, sort alphabetically'
+        description: 'Players, sort alphabetically',
+        keywords: ['sort', 'reduce', 'useState', 'useEffect']
     },{
         title: "Challenge 2",
         location: '/challenge2',
-        description: 'Players, sort by letter'
+        description: 'Players, sort by letter',
+        keywords: ['sort', 'reduce', 'useState', 'useEffect', 'map']
 
     },{
         title: "Challenge 3",
         location: '/challenge3',
-        description: 'Players, sort by team (color-coded)'
+        description: 'Players, sort by team (color-coded)',
+        keywords: ['sort', 'reduce', 'map', ]
     },{
         title: "Challenge 4",
         location: '/challenge4',
-        description: 'Create an archive containing previous challenges'
+        description: 'Create an archive containing previous challenges',
+        keywords: ['map']
     }
 ]
 
