0989409315433ef654087a7e03bfa89a7ed841c7
Crystal Knapp <rc_knapp@msn.com>
2022-08-24 01:06:01 -0400

    updated instructions

--- src/components/Challenge5.js
+++ src/components/Challenge5.js
@@ -1,20 +1,25 @@
 /*
     *** Challenge 5 ***
     For this challenge, you will create a search feature
-    for the archives
+    for the archives (based on a keyword list)
     
     When the user clicks on a search-by-keyword button, it 
     should return a list of all the keywords in all the 
     challenges. The list should only one of each keyword (no
-    duplicates).  *** note that there is now a keyword array
-    in the ./data/archives.js file for this purpose. there is 
-    no need to search the actual code ***
+    duplicates).  
+        *** note that there is now a keywords array
+        in the ./data/archives.js file for this purpose. There is 
+        no need to search the actual code ***
+        ---> optional: alphabetize the results so it's easier
+        for the user to find the keyword they are looking for
+
 
     The keywords (or phrases) displayed on the screen 
     should be clickable. Then, when the user clicks on 
     the keyword, it should then compile a list of every 
     challenge that has that keyword.
 
-    Update App.css as needed. 
+    Update App.css as needed. Feel free to have fun with 
+    the look of the results
 
 */
