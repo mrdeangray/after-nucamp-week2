d943b25048709b0c33085c6b852b1d5663968b99
mrdeangray <mrdeangray@gmail.com>
2022-08-28 20:50:29 -0400

    removed solution

--- src/pages/Active.js
+++ src/pages/Active.js
@@ -1,10 +1,10 @@
 // import Challenge6 from "../components/Challenge6"
-import Challenge8 from "../components/Challenge8"
+
 
 const Active = () => {
     return (
         <div>
-            <Challenge8 />
+
         </div>
     )
 }
