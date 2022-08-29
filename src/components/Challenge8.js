ee8c52ac4b5fe5888137b785c824dff0787ba80a
mrdeangray <mrdeangray@gmail.com>
2022-08-28 20:46:52 -0400

    added instructions

--- src/components/Challenge8.js
+++ src/components/Challenge8.js
@@ -1,4 +1,15 @@
-//Using of src attribute in this way means, your image will be loaded from the absolute path "///images/resto.png" for your site. Images directory should be located at the root of your site. 
+
+//Challenge8:
+//Create a Challenge8 component in the components directory.
+//The component should retrieve and display the names and images from the players.js file in the data directory.
+//The component should have two columns: Active and Inactive
+//Clicking each image in the Active column will move the player to the Inactive column
+//Clicking each image in the Inactive column will not do anything(for this challenge)
+//Both active and inactive columns should keep a count of their players.
+//Add CSS rules for the hover state of the players in the active column.
+//add a CSS rule to the App.css file so the component's body is indented.
+//Add a route/link for this component to the Header component.
+
 
 import { useState, useCallback, useEffect } from 'react'
 import playersData from '../data/players'
