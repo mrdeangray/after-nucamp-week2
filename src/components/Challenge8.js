34450bdc1958eb76fb638e624551ca0e00bdd301
mrdeangray <mrdeangray@gmail.com>
2022-08-30 18:34:42 -0400

    completed challenge8

--- src/components/Challenge8.js
+++ src/components/Challenge8.js
@@ -4,7 +4,7 @@
 //The component should retrieve and display the names and images from the players.js file in the data directory.
 //The component should have two columns: Active and Inactive
 //Clicking each image in the Active column will move the player to the Inactive column
-//Clicking each image in the Inactive column will not do anything(for this challenge)
+//Clicking each image in the Inactive column will move the player back to the Active column
 //Both active and inactive columns should keep a count of their players.
 //Add CSS rules for the hover state of the players in the active column.
 //add a CSS rule to the App.css file so the component's body is indented.
