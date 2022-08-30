3741fe2f791a31467039f91dfe26b831e91ab75e
mrdeangray <mrdeangray@gmail.com>
2022-08-30 17:59:15 -0400

    completed challenge8

--- src/components/Challenge8.js
+++ src/components/Challenge8.js
@@ -15,59 +15,50 @@ import { useState, useCallback, useEffect } from 'react'
 import playersData from '../data/players'
 
 const Challenge8 = () => {
-    const [players, setPlayers] = useState([...playersData])
+    const [activePlayers, setActivePlayers] = useState([...playersData])
     const [inactivePlayers, setInactivePlayers] = useState([])
-    const [activePlayerCount, setActivePlayerCount] = useState(players.length)
-    const [inactivePlayerCount, setInactivePlayerCount] = useState(inactivePlayers.length)
-    const [displayInactive, SetDisplayInactive] = useState(<div>No inactive players</div>)
+    const [displayInactive, setDisplayInactive] = useState(<div>No inactive players</div>)
+
+    const moveToInactive = (id) => {
+        setActivePlayers(activePlayers.filter((player) => player.id !== id))
+        const inactive = activePlayers.filter((player) => player.id === id)
+        setInactivePlayers([...inactivePlayers, ...inactive])
+    }
+
+    const moveToActive = useCallback((id) => {
+        setInactivePlayers(inactivePlayers.filter((player) => player.id !== id))
+        const active = inactivePlayers.filter((player) => player.id === id)
+        setActivePlayers([...active, ...activePlayers])
+    }, [activePlayers, inactivePlayers])
 
     useEffect(() => {
-        setActivePlayerCount(players.length)
-        setInactivePlayerCount(inactivePlayers.length)
-        SetDisplayInactive(
-
+        setDisplayInactive(
             inactivePlayers.map((player, idx) => (
-                <li className='#'
+                <li className='inactive-player'
                     key={idx}
-
+                    onClick={() => moveToActive(player.id)}
                 >
                     {player.name}
                     <img className="player-image" src={`/img-players/${player.image}`} alt="" />
                 </li>
             ))
-
         )
-    }, [inactivePlayers, players])
+    }, [inactivePlayers, activePlayers, moveToActive])
 
-
-    const handleClick = (id) => {
-        //console.log(evt)
-        console.log(id)
-        setPlayers(
-            players.filter((player) => player.id !== id)
-        )
-        console.log(inactivePlayers)
-        const inactive = players.filter((player) => player.id === id)
-        setInactivePlayers([...inactivePlayers, ...inactive])
-
-    }
 
 
 
     return (
-
-
         <div className='challenge8'>
             <h2>Challenge8</h2>
             <div className='grid-container'>
-
                 <div className='active'>
-                    <h4>Active Players {activePlayerCount}</h4>
+                    <h4>Active Players {activePlayers.length}</h4>
                     {
-                        players.map((player) => (
-                            <li className='challenge8-player'
+                        activePlayers.map((player) => (
+                            <li className='active-player'
                                 key={player.id}
-                                onClick={() => handleClick(player.id)}
+                                onClick={() => moveToInactive(player.id)}
                             >
                                 {player.name}
                                 <img className="player-image" src={`/img-players/${player.image}`} alt="" />
@@ -76,9 +67,8 @@ const Challenge8 = () => {
                     }
                 </div>
                 <div className='inactive'>
-                    <h4>Inactive Players: {inactivePlayerCount}</h4>
+                    <h4>Inactive Players: {inactivePlayers.length}</h4>
                     {displayInactive}
-
                 </div>
             </div>
         </div>
