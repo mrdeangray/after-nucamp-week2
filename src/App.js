124d7bb47d669b98282a74a114e62d7ff38d1488
mrdeangray <mrdeangray@gmail.com>
2022-09-03 12:29:19 -0400

    changed to App.-Dean.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App-Dean.css';
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
