64354275f29f63ff58ebd3e87bb246bfab52d1ad
Crystal Knapp <rc_knapp@msn.com>
2022-09-09 22:41:41 -0400

    11a added to allow rearranging order of dnd

--- src/pages/Active.js
+++ src/pages/Active.js
@@ -2,13 +2,14 @@
 
 import Challenge10Dean from "../components/Challenge10-Dean"
 import Challenge11 from "../components/Challenge11"
+import Challenge11a from "../components/Challenge11a"
 // import Challenge9 from "../components/Challenge9-Dean"
 
 
 const Active = () => {
     return (
         <div>
-            <Challenge11 />
+            <Challenge11a />
         </div>
     )
 }
